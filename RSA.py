aa8cf8ea05d5c3bd332184940df6040261fa9407
billonalex <billonalex21@gmail.com>
2020-11-25 09:46:46 +0100

    [Add] RSA keygen function

---
+++ RSA.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 09:43:48 2020
+
+@author: Alexandre
+"""
+
+def generate_RSA(bits=2048):
+    '''
+    Generate an RSA keypair with an exponent of 65537 in PEM format
+    param: bits The key length in bits
+    Return private key and public key
+    '''
+    from Crypto.PublicKey import RSA 
+    new_key = RSA.generate(bits, e=65537) 
+    public_key = new_key.publickey().exportKey("PEM") 
+    private_key = new_key.exportKey("PEM") 
+    return private_key, public_key
+
+priv, pub = generate_RSA()
+
+print(priv)
+
+print(pub)
+
